3677711222b8fa53c614e40a10bfafce0a02e622
Jon Elias <jon39334@gmail.com>
2015-10-15 15:57:22 +0200

    commented the first loop

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import os
 graph = nx.Graph()
 
 current = "/" # current path
-currentfolders = [] #all folders in the current folder
+
 going = True
 while going=True:
 
-	for file in os.listdir(current):
+	for file in os.listdir(current): # disk folder loop
 		fullpath = current+"/"+file
 		if os.path.isdir(fullpath):
 			currentfolders.append(file)
 		
 		graph.add_edge(current, file)
-	
+
 	
