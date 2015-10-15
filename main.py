181b21d8b21266caac8e434b6218e274ab595ee0
Jon Elias <jon39334@gmail.com>
2015-10-15 18:37:35 +0200

    empty handling
    when it has searched so deep that there is not any folders left, it will
    not go up 1 folder and continue plotting from there

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import random
 
 class computerplot(object):
 	def __init__(self):
-		self.graph = nx.Graph()
+		self.graph = nx.Graph() # ze ram eater
 		self.current = "/" # current path
-		
+	
 	def main(self):
 		going = True
 		self.folderScan = {} # where os.listdir() content goes
@@ -14,23 +14,33 @@ class computerplot(object):
 		
 		while going = True:
 			currentFolders = self.populate(fullpath)
-			fullpath += "/" + random.choise( currentFolders ) # random next folder
- 	
-	def graph(x,y): # x is from (fullpath), y is to (filename)
-		folders = x.split()
-		self.graph.add_edge( folders[ len(folders)-1], y) # last element in folders is the upper foldername
+			
+			if currentFolder: 
+				fullpath += "/" + random.choise( currentFolders ) # random next folder
+			else: # if empty
+				del self.folderscan[fullpath][ self.folderscan[fullpath].index(fullpath) ]
+				# deleting the occurance of done folder in the folderscan entry of the Upperfolder. So that it wont be plotted twice
+			
+			
+	def getUpperFolder(self, fullpath):
+		folders = x.split("/")
+		return folders[ len(folders)-1 ]
+		
+	def graph(self, x, y): # x is from (fullpath), y is to (filename)
+		self.graph.add_edge( self.getUpperFolder(x), y) # last element in folders is the upper foldername
 		
 	def populate(self, fullpath):
-		files = os.listdir()
+		files = os.listdir() # list all files and folders
+		
 		for file in files:
-			
-			filePath = fullpath+"/"+file
+			filePath = fullpath+"/"+file # path to the current file
 			
 			self.graph(fullPath, file) # network graph them
 			
-			if not os.path.isdir(filePath): 
+			if not os.path.isdir(filePath): # if not directory
 				del file
 		
-		self.folderScan[fullpath] = files
+		if files: # if not empty
+			self.folderScan[fullpath] = files
 		return files
-		
+			
