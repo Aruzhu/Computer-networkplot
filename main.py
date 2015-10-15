cb64a849483c22170dd3fb1c55ca4e381aa1656e
Jon Elias <jon39334@gmail.com>
2015-10-15 17:15:31 +0200

    put everything in a class instead

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import networkx as nx
 import os
-
-graph = nx.Graph()
-
-current = "/" # current path
-
-going = True
-while going=True:
-
-	for file in os.listdir(current): # disk folder loop
-		fullpath = current+"/"+file
-		if os.path.isdir(fullpath):
-			currentfolders.append(file)
+class computerplot(object):
+	def __init__(self):
+		self.graph = nx.Graph()
+		self.current = "/" # current path
 		
-		graph.add_edge(current, file)
-
+	def main(self):
+		going = True
+		self.folderScan = {}
+		
+		while going=True:
 	
+	def graph(x,y): # x is from (fullpath), y is to (filename)
+		folders = x.split()
+		self.graph.add_edge( folders[ len(folders)], y) # last element in folders is the upper foldername
+		
+	def populate(self, fullpath):
+		files = os.listdir()
+		for file in files:
+			
+			filePath = fullpath+"/"+file
+			if not os.path.isdir(filePath): #is file
+			
+				graph(fullPath, file) # graph them
+				del files
+		
+		
