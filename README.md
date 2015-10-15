44ae30c5f9b6755f739bcbb7eb8fd1dbecc62acc
Jon Elias <jon39334@gmail.com>
2015-10-15 17:54:38 +0200

    finished the populate class, and made graph class
    also edited readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Computer-networkplot
 This when it is finished will go through all the files on your computer and make a network graph out of your entire computer. In the graph nodes are folders or files.
+I must save every folderscan i do, because i have to make a folder disappear 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import networkx as nx
 import os
+import random
+
 class computerplot(object):
 	def __init__(self):
 		self.graph = nx.Graph()
@@ -7,22 +9,28 @@ class computerplot(object):
 		
 	def main(self):
 		going = True
-		self.folderScan = {}
+		self.folderScan = {} # where os.listdir() content goes
+		fullpath = "/"
 		
-		while going=True:
-	
+		while going = True:
+			currentFolders = self.populate(fullpath)
+			fullpath += "/" + random.choise( currentFolders ) # random next folder
+ 	
 	def graph(x,y): # x is from (fullpath), y is to (filename)
 		folders = x.split()
-		self.graph.add_edge( folders[ len(folders)], y) # last element in folders is the upper foldername
+		self.graph.add_edge( folders[ len(folders)-1], y) # last element in folders is the upper foldername
 		
 	def populate(self, fullpath):
 		files = os.listdir()
 		for file in files:
 			
 			filePath = fullpath+"/"+file
-			if not os.path.isdir(filePath): #is file
 			
-				graph(fullPath, file) # graph them
-				del files
+			self.graph(fullPath, file) # network graph them
+			
+			if not os.path.isdir(filePath): 
+				del file
 		
+		self.folderScan[fullpath] = files
+		return files
 		
