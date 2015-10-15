2553b2cbebbfaed2853f975bf4bf497ad82b96f0
Jon Elias <jon39334@gmail.com>
2015-10-15 18:54:28 +0200

    runned it for the first time
    Cleaned up syntax errors

--- main.py
+++ main.py
@@ -12,30 +12,29 @@ class computerplot(object):
 		self.folderScan = {} # where os.listdir() content goes
 		fullpath = "/"
 		
-		while going = True:
+		while going == True:
 			currentFolders = self.populate(fullpath)
-			
-			if currentFolder: 
-				fullpath += "/" + random.choise( currentFolders ) # random next folder
+			print currentFolders
+			if currentFolders: 
+				fullpath += "/" + random.choice( currentFolders ) # random next folder
 			else: # if empty
 				del self.folderscan[fullpath][ self.folderscan[fullpath].index(fullpath) ]
 				# deleting the occurance of done folder in the folderscan entry of the Upperfolder. So that it wont be plotted twice
 			
-			
 	def getUpperFolder(self, fullpath):
-		folders = x.split("/")
+		folders = fullpath.split("/")
 		return folders[ len(folders)-1 ]
 		
-	def graph(self, x, y): # x is from (fullpath), y is to (filename)
+	def graphFunc(self, x, y): # x is from (fullpath), y is to (filename)
 		self.graph.add_edge( self.getUpperFolder(x), y) # last element in folders is the upper foldername
 		
 	def populate(self, fullpath):
-		files = os.listdir() # list all files and folders
+		files = os.listdir(fullpath) # list all files and folders
 		
 		for file in files:
 			filePath = fullpath+"/"+file # path to the current file
 			
-			self.graph(fullPath, file) # network graph them
+			self.graphFunc(fullpath, file) # network graph them
 			
 			if not os.path.isdir(filePath): # if not directory
 				del file
@@ -43,4 +42,6 @@ class computerplot(object):
 		if files: # if not empty
 			self.folderScan[fullpath] = files
 		return files
-			
+computerplot = computerplot()
+computerplot.__init__()
+computerplot.main()
